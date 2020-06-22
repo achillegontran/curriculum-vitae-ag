42c6712db2ced6d1284fe9773f50c672a3887475
achillegontran <67260361+achillegontran@users.noreply.github.com>
2020-06-22 13:19:25 +0200

    Ajoute d'icon
    Ajoute d'icon

--- src/components/MonCV/MonCV.vue
+++ src/components/MonCV/MonCV.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="MonCV">
-        <div class="container bg-light">
+        <div class="container">
             <div class="row">
                 <div class="col-8">
                     <h1 class="text-left">Achille Gontran</h1>
-                    <p class="text-left">Chemin de l'Alouette 1, 1110 Morges</p>
-                    <p class="text-left"><a href="mailto:gontran.achille@gmail.com">gontran.achille@gmail.com</a></p>
-                    <p class="text-left"><a class="text-left" href="tel:+41766936642">076 693 66 42</a></p>
+                    <p class="text-left"><i class="fas fa-map-marker-alt"></i> Chemin de l'Alouette 1, 1110 Morges</p>
+                    <p class="text-left"><a href="mailto:gontran.achille@gmail.com"><i class="far fa-envelope"></i> gontran.achille@gmail.com</a></p>
+                    <p class="text-left"><a class="text-left" href="tel:+41766936642"><i class="fas fa-phone-alt"></i> 076 693 66 42</a></p>
                     <p class="text-left">Né le 12 décembre 1985</p>
                     <p class="text-left">Mauricien</p>
                 </div>
                 <div class="col-4">
-                    <img src="../../assets/achille.jpg" alt="achille gontran"/>
+                    <img src="../../assets/achille.jpg" alt="achille gontran" />
                 </div>
             </div>
             <div class="row">
@@ -20,86 +20,89 @@
 
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-12">
-                    <h3 class="text-left">Formation</h3>
+                    <h3 class="text-left"><i class="fas fa-graduation-cap"></i> Formation</h3>
                     <hr>
                     <div class="row">
                         <div class="col text-left">2019-ce jour</div>
                         <div class="col text-left">Bachelor Informaticien de gestion</div>
-                        <div class="col text-left">HE-ARC, Neuchâtel, Suisse</div>
+                        <div class="col text-left font-italic text-muted">HE-ARC, Neuchâtel, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2016-2019</div>
                         <div class="col text-left">Informaticien de gestion</div>
-                        <div class="col text-left">CPLN, Neuchâtel, Suisse</div>
+                        <div class="col text-left font-italic text-muted">CPLN, Neuchâtel, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2012-2016</div>
                         <div class="col text-left">CFC Informaticien</div>
-                        <div class="col text-left">Ecole des Arches, Lausanne, Suisse</div>
+                        <div class="col text-left font-italic text-muted">Ecole des Arches, Lausanne, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2005-2006</div>
                         <div class="col text-left">Ecole hôtelière</div>
-                        <div class="col text-left">Hocatel, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">Hocatel, île Maurice</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">1998-2004</div>
                         <div class="col text-left">GCE final examination graded</div>
-                        <div class="col text-left">City College, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">City College, île Maurice</div>
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-12">
-                    <h3 class="text-left">Expérience professionnelle</h3>
+                    <h3 class="text-left"><i class="fas fa-toolbox"></i> Expérience professionnelle</h3>
                     <hr>
                     <div class="row">
                         <div class="col text-left">2016-ce jour</div>
                         <div class="col text-left">Web developer</div>
-                        <div class="col text-left">STS SA, Lausanne, Suisse</div>
+                        <div class="col text-left font-italic text-muted">STS SA, Lausanne, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2014-2016</div>
                         <div class="col text-left">Stagiaire en informatique</div>
-                        <div class="col text-left">STS SA, Lausanne, Suisse</div>
+                        <div class="col text-left font-italic text-muted">STS SA, Lausanne, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2011-2012</div>
                         <div class="col text-left">Night Chef</div>
-                        <div class="col text-left">LUX, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">LUX, île Maurice</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2010-2011</div>
                         <div class="col text-left">Au pair</div>
-                        <div class="col text-left">André Bucher, Morges, Suisse</div>
+                        <div class="col text-left font-italic text-muted">André Bucher, Morges, Suisse</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2008-2010</div>
                         <div class="col text-left">Demi-Chef de partie</div>
-                        <div class="col text-left">Cocoloco restaurant, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">Cocoloco restaurant, île Maurice</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2008</div>
                         <div class="col text-left">Cuisinier</div>
-                        <div class="col text-left">Pavillon Hotel, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">Pavillon Hotel, île Maurice</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2007-2009</div>
                         <div class="col text-left">Cuisinier</div>
-                        <div class="col text-left">Restaurant 21-22, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">Restaurant 21-22, île Maurice</div>
                     </div>
                     <div class="row">
                         <div class="col text-left">2005-2007</div>
                         <div class="col text-left">Assistant cuisinier</div>
-                        <div class="col text-left">Coin de Mire Hotel, île Maurice</div>
+                        <div class="col text-left font-italic text-muted">Coin de Mire Hotel, île Maurice</div>
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-12">
-                    <h3 class="text-left">Connaissances linguistique</h3>
+                    <h3 class="text-left"><i class="fas fa-globe"></i> Connaissances linguistique</h3>
                     <hr>
                     <div class="row">
                         <div class="col text-left">Créole</div>
@@ -115,9 +118,10 @@
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-12">
-                    <h3 class="text-left">Compétences et qualités</h3>
+                    <h3 class="text-left"><i class="fas fa-award"></i> Compétences et qualités</h3>
                     <hr>
                     <div class="row">
                         <div class="col text-left">HTML/CSS</div>
@@ -175,12 +179,25 @@
                             </div>
                         </div>
                     </div>
+                    <br>
                     <div class="row">
-                        <div class="col text-left">Gestion du personnel et stock, autonomie, capacité à travailler sous stress et en équipe, perfectionnisme</div>
+                        <div class="col">Gestion du personnel et stock, autonomie, capacité à travailler sous stress et en équipe, perfectionnisme</div>
                     </div>
                 </div>
             </div>
-
+            <br>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-left"><i class="fas fa-network-wired"></i> Reseaux sociaux</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col"><a href=""><i class="fab fa-google-plus-g"></i></a></div>
+                        <div class="col"><a href=""><i class="fab fa-facebook-square"></i></a></div>
+                        <div class="col"><a href=""><i class="fab fa-linkedin-in"></i></a></div>
+                    </div>
+                </div>
+            </div>
+            <br>
 
         </div>
     </div>
