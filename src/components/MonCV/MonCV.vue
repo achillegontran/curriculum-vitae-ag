3b4cc79c611c7607a5bb06167002221027db07ba
achillegontran <67260361+achillegontran@users.noreply.github.com>
2020-06-22 11:21:03 +0200

    Semantic HTML

---
+++ src/components/MonCV/MonCV.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="MonCV">
+        <div class="container bg-light">
+            <div class="row">
+                <div class="col-8">
+                    <h1 class="text-left">Achille Gontran</h1>
+                    <p class="text-left">Chemin de l'Alouette 1, 1110 Morges</p>
+                    <p class="text-left"><a href="mailto:gontran.achille@gmail.com">gontran.achille@gmail.com</a></p>
+                    <p class="text-left"><a class="text-left" href="tel:+41766936642">076 693 66 42</a></p>
+                    <p class="text-left">Né le 12 décembre 1985</p>
+                    <p class="text-left">Mauricien</p>
+                </div>
+                <div class="col-4">
+                    <img src="../../assets/achille.jpg" alt="achille gontran"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h2>Web Developer</h2>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-left">Formation</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col text-left">2019-ce jour</div>
+                        <div class="col text-left">Bachelor Informaticien de gestion</div>
+                        <div class="col text-left">HE-ARC, Neuchâtel, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2016-2019</div>
+                        <div class="col text-left">Informaticien de gestion</div>
+                        <div class="col text-left">CPLN, Neuchâtel, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2012-2016</div>
+                        <div class="col text-left">CFC Informaticien</div>
+                        <div class="col text-left">Ecole des Arches, Lausanne, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2005-2006</div>
+                        <div class="col text-left">Ecole hôtelière</div>
+                        <div class="col text-left">Hocatel, île Maurice</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">1998-2004</div>
+                        <div class="col text-left">GCE final examination graded</div>
+                        <div class="col text-left">City College, île Maurice</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-left">Expérience professionnelle</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col text-left">2016-ce jour</div>
+                        <div class="col text-left">Web developer</div>
+                        <div class="col text-left">STS SA, Lausanne, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2014-2016</div>
+                        <div class="col text-left">Stagiaire en informatique</div>
+                        <div class="col text-left">STS SA, Lausanne, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2011-2012</div>
+                        <div class="col text-left">Night Chef</div>
+                        <div class="col text-left">LUX, île Maurice</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2010-2011</div>
+                        <div class="col text-left">Au pair</div>
+                        <div class="col text-left">André Bucher, Morges, Suisse</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2008-2010</div>
+                        <div class="col text-left">Demi-Chef de partie</div>
+                        <div class="col text-left">Cocoloco restaurant, île Maurice</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2008</div>
+                        <div class="col text-left">Cuisinier</div>
+                        <div class="col text-left">Pavillon Hotel, île Maurice</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">2007-2009</div>
+                        <div class="col text-left">Cuisinier</div>
+                        <div class="col text-left">Restaurant 21-22, île Maurice</div>
+                    </div>
+                    <div class="row">
+                        <div class="col">2005-2007</div>
+                        <div class="col">Assisstant cuisinier</div>
+                        <div class="col">Coin de Mire Hotel, île Maurice</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-left">Connaissances linguistique</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col text-left">Créole</div>
+                        <div class="col text-left">Langue maternelle</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">Français</div>
+                        <div class="col text-left">Parlé, lu, écrit</div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-left">Anglais</div>
+                        <div class="col text-left">Parlé, lu, écrit</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-left">Compétences et qualités</h3>
+                    <hr>
+                    <div class="row">
+                        <div class="col text-left">Gestion du personnel et stock, autonomie, capacité à travailler sous stress et en équipe, perfectionnisme</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MonCV',
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import $ from"jquery";
+window.$ = $;
+import"bootstrap";
+import"bootstrap/dist/css/bootstrap.min.css";
+//import"bootswatch/dist/darkly/bootstrap.min.css";
+import"@fortawesome/fontawesome-free/css/all.min.css";
+
+
+$(document).ready(() => {console.log("it works!");});
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
